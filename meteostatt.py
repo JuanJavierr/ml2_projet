c69bee783f2635d51f10b3c830add39e5691de0e
JuanJavierr <xavierpeladeau12@hotmail.com>
2024-11-23 14:15:07 -0500

    fill ALL temp missing values

--- meteostatt.py
+++ meteostatt.py
@@ -4,9 +4,12 @@ from meteostat import Monthly, Point
 from datetime import datetime
 
 df = pd.read_csv("geolocation.csv", index_col=0)
+df = df.reset_index()
+df = df.rename(columns={"index": "mrc"})
+
 # %%
-result = []
-for i, row in df.iterrows():
+result = pd.DataFrame()
+for i,  row in df.iterrows():
 
     location = Point(row["lat"], row["lng"])
 
@@ -14,10 +17,18 @@ for i, row in df.iterrows():
     data = Monthly(location, start=datetime.strptime("2016-01-01", "%Y-%m-%d"), end=datetime.strptime("2023-12-31", "%Y-%m-%d"))
 
     data = data.fetch()
-    data["mrc"] = i
+    # print(i, index)
+    data["mrc"] = row["mrc"]
+    data["lat"] = row["lat"]
+    data["lng"] = row["lng"]
 
-    result.append(data)
+    if data.empty:
+        print(f"Empty data for {row['mrc']}")
+        data = pd.DataFrame({"mrc": row["mrc"], "lat": row["lat"], "lng": row["lng"], "tavg": [None]}, index=pd.date_range(start="2016-01-01", end="2023-12-31", freq="MS"))
 
+    result = pd.concat([result, data])
+
+result["is_missing"] = result["tavg"].isna()
 
 # %%
 
@@ -25,42 +36,79 @@ for i, row in df.iterrows():
 import geopandas as gpd
 
 gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.lng, df.lat))
+
+# Remove MRCs for which we don't have temp data
+# gdf = gdf[~gdf.index.isin(["Témiscouata", "Caniapiscau", "Manicouagan", "Avignon", "Antoine-Labelle"])]
+
 gdf = gdf.set_crs("EPSG:4326")
 gdf = gdf.to_crs("EPSG:2950")
 
-for i, row in df.iterrows():
+# %%
+for i, mrc in gdf.iterrows():
     dist_from_mrc = gdf.distance(gdf.loc[i, "geometry"]).sort_values()
-    closest_mrc = dist_from_mrc[1:].index[0]
-    second_closest_mrc = dist_from_mrc[1:].index[1]
+    indexes_of_closest_mrc = list(dist_from_mrc.index[1:4])
 
-    df.loc[i, "closest_mrc"] = closest_mrc
-    df.loc[i, "second_closest_mrc"] = second_closest_mrc
-    df.loc[i, "third_closest_mrc"] = dist_from_mrc[1:].index[2]
+    index_to_name = dict(zip(gdf.index, gdf.mrc))
+    gdf.loc[i, "closest_mrc"] = index_to_name[indexes_of_closest_mrc[0]]
+    gdf.loc[i, "second_closest_mrc"] = index_to_name[indexes_of_closest_mrc[1]]
+    gdf.loc[i, "third_closest_mrc"] = index_to_name[indexes_of_closest_mrc[2]]
+
 # %%
 # Replace missing values with closest MRC
-for i,(mrc, row) in enumerate(df.iterrows()):
-    data = result[i]
-    closest_mrc = df.loc[mrc, "closest_mrc"]
-    second_closest_mrc = df.loc[mrc, "second_closest_mrc"]
+print(result.isna().sum())
+filled_result = result.copy()
+for i, row in result.iterrows():
+    if pd.isna(row["tavg"]):
+        closest_mrc = gdf[gdf['mrc'] == row["mrc"]]["closest_mrc"].iloc[0]
+        second_closest_mrc = gdf[gdf['mrc'] == row["mrc"]]["second_closest_mrc"].iloc[0]
+        third_closest_mrc = gdf[gdf['mrc'] == row["mrc"]]["third_closest_mrc"].iloc[0]
 
-    closest_mrc_row_nb = df.index.get_loc(closest_mrc)
-    closest_data = result[closest_mrc_row_nb]
+        closest_mrc_temp = None
+        if closest_mrc is not None and pd.isna(closest_mrc_temp):
+            closest_mrc_temp = result.loc[(result["mrc"] == closest_mrc) & (result.index == i), "tavg"].mean()
+        if second_closest_mrc is not None and pd.isna(closest_mrc_temp):
+            closest_mrc_temp = result.loc[(result["mrc"] == second_closest_mrc) & (result.index == i), "tavg"].mean()
+        if third_closest_mrc is not None and pd.isna(closest_mrc_temp):
+            closest_mrc_temp = result.loc[(result["mrc"] == third_closest_mrc) & (result.index == i), "tavg"].mean()
+        if pd.isna(closest_mrc_temp):
+            closest_mrc_temp = result.loc[(result["mrc"] == "Montcalm") & (result.index == i), "tavg"].mean()
+        if pd.isna(closest_mrc_temp):
+            print(f"No closest MRC found for {row['mrc']}, index {i}")
+            raise ValueError("No closest MRC found")
+        filled_result.loc[i, "tavg"] = closest_mrc_temp
 
-    second_closest_mrc_row_nb = df.index.get_loc(second_closest_mrc)
-    second_closest_data = result[second_closest_mrc_row_nb]
+print(filled_result.isna().sum())
 
-    third_closest_mrc = df.loc[mrc, "third_closest_mrc"]
-    third_closest_mrc_row_nb = df.index.get_loc(third_closest_mrc)
-    third_closest_data = result[third_closest_mrc_row_nb]
-
-    for col in data.columns:
-        data[col] = data[col].fillna(closest_data[col])
-        data[col] = data[col].fillna(second_closest_data[col])
-        data[col] = data[col].fillna(third_closest_data[col])
-
-
-    result[i] = data
 # %%
 
-pd.concat(result).to_csv("monthly_avg_temp.csv")
+
+def fill_missing_values(temp):
+    from arima import load_data
+    import pandas as pd
+
+    df = load_data()
+    df = df.set_index(["MRC_TXT", "SECTEUR", pd.to_datetime(df.index, format="%Y-%m")])
+
+
+    temp = temp.set_index(["mrc", pd.to_datetime(temp.index)])
+    temp = temp[["tavg"]]
+
+
+    # change df index names
+    df.index.names = ["mrc", "sector", "date"]
+    temp.index.names = ["mrc", "date"]
+
+    joined_df = df.join(temp, how="left", on=["mrc", "date"])
+    joined_df = joined_df.drop(columns=["REGION_ADM_QC_TXT", "ANNEE_MOIS"])
+    # get smallest date in temp
+    avg_temp_per_month = temp.groupby("date").mean()
+    joined_df = joined_df.join(avg_temp_per_month, how="left", on="date", rsuffix="_avg")
+    joined_df["tavg"] = joined_df["tavg"].fillna(joined_df["tavg_avg"])
+    joined_df = joined_df.drop(columns=["tavg_avg"])
+
+    return joined_df
+
+final_df = fill_missing_values(filled_result)
+final_df.to_csv("dataset.csv")
+
 # %%
